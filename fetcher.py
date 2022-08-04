d5655b285d684ad4fb75c9737397ff00981ef9ea
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2022-08-04 21:05:30 +0530

    .

---
+++ fetcher.py
@@ -0,0 +1,75 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+import os
+import html
+#college = {"AI": 63, "AD": 62, "CS": 187, "IS": 191, "ME": 17, "CV": 43, "AE": 54, "EE": 47, "EC": 179}
+#college = {"AI": 3, "AD": 2, "CS": 1, "IS": 2, "ME": 2, "CV": 2, "AE": 2, "EE": 2, "EC": 2}
+college = {"CS": 1}
+CAPCHA = '09u868v5e5a261vov4403hnigvpn8gs0umrjjlogalnsbgtbchil43clf2fvauu45qkcpsrbsllc6n18pa3u5jlv5ods4g9kj8fo6j0'
+COOKIE = 'WVSBT5'
+TOKEN = 'bd14e8bf0acaafe291f81c2a94b28d324ac86c67'
+URL = "https://results.vtu.ac.in/FMEcbcs22/FMEcbcs22/resultpage.php"
+HEADERS = {
+    'Host': 'results.vtu.ac.in',
+    'Cookie': 'VISRE='+COOKIE,
+    'Content-Length': '80',
+    'Cache-Control': 'max-age=0',
+    'Sec-Ch-Ua': '" Not A;Brand";v="99", "Chromium";v="96"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Linux"',
+    'Upgrade-Insecure-Requests': '1',
+    'Origin': 'https://results.vtu.ac.in',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document',
+    'Referer': 'https://results.vtu.ac.in/FMEcbcs22/index.php',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Connection': 'close'
+}
+exam = input("Enter Semester Number : ") + " Semester"
+#exam = str(1)+ " Semester"
+path = os.getcwd()
+#with pd.ExcelWriter(exam+".xlsx") as writer:
+for course, nos in college.items():
+    name = [np.nan]*nos
+    usno = [np.nan]*nos
+    cr = [np.nan]*nos
+    co = [np.nan]*nos
+    sgpa = [np.nan]*nos
+    grade = [np.nan]*nos
+    for n in range(0,nos):
+        usn = '1DB21' + course + str(n+1).zfill(3)
+        PAYLOAD = 'Token='+TOKEN+'&lns='+usn+'&captchacode='+CAPCHA
+        r = requests.post(url = URL, headers= HEADERS, data=PAYLOAD)
+        htl = html.unescape(r.text)
+        cleantext = BeautifulSoup(htl, "html.parser").text
+        text = "\n".join([ll.rstrip() for ll in cleantext.splitlines() if ll.strip()])
+        text = text.split("\n")
+        name[n] = (text[4])
+        #print(text[4])
+        #print(text[5])
+        #print(text[8])
+        #print(text[10])
+        #print(text[12])
+        #print("PASS" if text[10] == "21" else "FAIL")
+        #usno[n] = (text[5])
+        #cr[n] = int(text[8])
+        #co[n] = int(text[10])
+        #sgpa[n] = float(text[12])
+        #grade[n] = ("PASS" if text[10] == "21" else "FAIL")
+    #df = pd.DataFrame({
+    #    'USN':usno,
+    #    'Name':name,
+    #    'Credits Required':cr,
+    #    'Credits Obtained':co,
+    #    'SGPA':sgpa,
+    #    'Grade':grade,
+    #})
+    #df.to_excel(writer, sheet_name=course, index=False,)
